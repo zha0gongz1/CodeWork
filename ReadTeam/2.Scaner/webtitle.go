b7dd7d93b00556a482ad4eb6e7503ed7e247b794
赵公子 <zha0gongz1@protonmail.com>
2023-05-19 16:04:54 +0800

    Create webtitle.go

---
+++ ReadTeam/2.Scaner/webtitle.go
@@ -0,0 +1,68 @@
+//多线程且互斥输出
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+	"sync"
+	"time"
+)
+
+
+func main() {
+	start := time.Now()
+	defer func() {
+		cost := time.Since(start)
+		fmt.Println("\ncost=", cost)
+	}()
+	urls := []string{
+		"https://www.google.com",
+		"https://www.example.net",
+		"https://github.com/",
+		"https://github.com/zha0gongz1",
+		"https://shopee.sg/",
+    "https://www.baidu.com",
+		"https://www.cnblogs.com/H4ck3R-XiX",
+	}
+
+	var wg sync.WaitGroup
+	var mutex sync.Mutex
+	ch := make(chan struct{}, 10) // 最多允许 10 个并发 goroutine
+	for _, url := range urls {
+		wg.Add(1)
+		go func(url string) {
+
+			defer wg.Done()
+			ch <- struct{}{}        // 获取令牌
+			defer func() { <-ch }() // 释放令牌
+			resp, err := http.Get(url)
+			if err != nil {
+				fmt.Printf("Error fetching: %s\n", url)
+				return
+			}
+			defer resp.Body.Close()
+			mutex.Lock()
+			re := regexp.MustCompile(`<title>(.*?)</title>`)
+			bodyBytes, _ := ioutil.ReadAll(resp.Body)
+			//if err != nil {
+			//	fmt.Printf("Error reading body: %s\n", url)
+			//	return
+			//}
+			//bodyString := string(bodyBytes)
+			match := re.FindStringSubmatch(string(bodyBytes))
+			if len(match) != 0 {
+				//fmt.Printf("Title: %s", match[1])
+				fmt.Printf("[%d] %s\tTitle:%s", resp.StatusCode, url, match[1])
+			} else {
+				fmt.Printf("[%d] %s\tTitle:NULL", resp.StatusCode, url)
+			}
+			//fmt.Printf("[%d] %s\tTitle:%s", resp.StatusCode, url, match[1])
+			fmt.Print("\n")
+      
+			mutex.Unlock()
+		}(url)
+	}
+	wg.Wait()
+}
